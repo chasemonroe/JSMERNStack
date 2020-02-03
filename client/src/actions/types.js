9f0a8dc850fb9332730e00553ba99b8bfb6a1d69
chasevisa <djchasevisa@gmail.com>
2020-02-02 19:57:56 -0500

    deploy to production

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -28,6 +28,13 @@ export const GET_REPOS= 'GET_REPOS';
 // Posts
 export const GET_POSTS = 'GET_POSTS';
 export const POST_ERROR = 'POST_ERROR';
+export const DELETE_POST = 'DELETE_POST';
+export const ADD_POST = 'ADD_POST';
+export const GET_POST = 'GET_POST';
 
 // Likes
 export const UPDATE_LIKES = 'UPDATE_LIKES';
+
+// Comments
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
